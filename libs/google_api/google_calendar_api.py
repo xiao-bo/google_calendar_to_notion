573cdf1efa542c9ac4dfe8ee80d6c634fad448d3
bowen <bowen@leju.com.tw>
2023-07-16 21:32:36 +0800

    feature:新增token過期的做法

--- libs/google_api/google_calendar_api.py
+++ libs/google_api/google_calendar_api.py
@@ -25,33 +25,28 @@ class GoogleCalendarAPI(object):
 
     def __get_creds(self) -> object:
         """
-        creds = None
         # The file token.json stores the user's access
         # and refresh tokens, and is
         # created automatically when the authorization
         # flow completes for the first
         # time.
         """
-        # todo 
-        # token過期的作法
 
+        creds = None
         if os.path.exists(self.__token_path):
             creds = Credentials.from_authorized_user_file(
                 self.__token_path, SCOPES)
-            return creds
 
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    self.__auth_path, SCOPES)
-                creds = flow.run_local_server(port=0)
+            print('認證過期，請重新授權')
+            flow = InstalledAppFlow.from_client_secrets_file(
+                self.__auth_path, SCOPES)
+            creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open(self.__token_path, 'w') as token:
                 token.write(creds.to_json())
-            return creds
+        return creds
 
     def query_event_from_calendar(
         self,
